561ccab9bf82b377700e269ff7be710cf424037d
Angelo Delicato <20096272+thelicato@users.noreply.github.com>
2025-04-30 10:42:33 +0200

    fix: use `GITHUB_TOKEN` as Bearer

--- scripts/parser.py
+++ scripts/parser.py
@@ -20,7 +20,7 @@ def get_github_headers():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3"
     }
     if github_token:
-        headers["Authorization"] = f"token {github_token}"
+        headers["Authorization"] = f"Bearer {github_token}"
     return headers
 
 def make_request(host, url, headers):
